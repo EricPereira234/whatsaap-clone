30346485096b3a83743174c8e868cc42520232a0
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2023-05-13 17:01:27 -0300

    chat

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,11 +1,35 @@
 import "./chatWindow.css";
+import { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
+
+import MessageItem from "./MessageItem";
 
 import {BsFillPersonPlusFill } from 'react-icons/bs';
 import {FiSearch } from 'react-icons/fi';
 import {FiMoreVertical } from 'react-icons/fi';
 import {FiPaperclip } from 'react-icons/fi';
+import {BsEmojiSmile } from 'react-icons/bs';
+import {AiOutlineSend} from 'react-icons/ai';
+import {AiOutlineClose} from 'react-icons/ai';
+import {BsFillMicFill} from 'react-icons/bs';
 
 export default function ChatWindow(){
+    const [emojiOpen, setEmojiOpen] = useState(false);
+    const [text, setText] = useState('');
+    const [list, setList] = useState([{},{}]);
+
+    const handleEmojiClick = ()=>{
+        setEmojiOpen(true);
+    }
+
+    const handleEmojiClickClose = ()=>{
+        setEmojiOpen(false);
+    }
+
+    const handleEmojiClickText = ({emoji})=>{
+        setText(text + emoji);
+    }
+
     return(
         <div className="chatWindow" >
             <div className="chat-header" >
@@ -24,12 +48,61 @@ export default function ChatWindow(){
 
             </div>
 
-            <div className="chat-body" >
 
+            <div className="chat-body" >
+                {list.map((item, key)=>(
+                    <MessageItem 
+                        key={key}
+                        data={item}
+                    />
+                ))}
+            </div>
+
+
+            <div className="chat-emojiarea">
+               {emojiOpen ? 
+                <EmojiPicker 
+                     searchDisabled
+                     width={'100%'}
+                     height={'300px'}
+                     onEmojiClick={handleEmojiClickText}
+                 />
+                 : <div></div>
+               }
             </div>
 
             <div className="chat-footer" >
+                <div className="footer-pre" >
+                    <BsEmojiSmile
+                        size={25}
+                        color="#919191"
+                        onClick={handleEmojiClick}
+                        style={!emojiOpen ? { color: "#919191" } : { color: "#00BFFF" }}
 
+                    />
+
+                    <AiOutlineClose size={25} color="#919191"
+                        style={emojiOpen ? { width: '25px' } : { width: '0px' }}
+                        onClick={handleEmojiClickClose}
+                    />
+                </div>
+
+                <div className="footer-area" >
+                    <input 
+                        type="text" 
+                        placeholder="Digite uma mensagem"
+                        value={text}
+                        onChange={(e)=>setText(e.target.value)}
+                    />
+                </div>
+
+                <div className="footer-pos" >
+                    {text ? 
+                     <AiOutlineSend size={25} color="#919191"/>
+                     :
+                     <BsFillMicFill size={25} color="#919191"/>
+                    }
+                </div>
             </div>
         </div>
         
---
+++ src/components/MessageItem/index.js
@@ -0,0 +1,12 @@
+import "./MessageItem.css";
+
+export default function MessageItem(){
+    return(
+        <div className="messageLine" >
+            <div className="messageItem" >
+                <div className="messageText" >Bla Bla...</div>
+                <div className="messageDate" > 19:00</div>
+            </div>
+        </div>
+    );
+}
